f91ddff172e1fd893ce194b628dee9aca494391d
Kiril <kiril_zafirov@yahoo.com>
2019-01-02 14:22:33 +0100

    added navigation and a dummy singleton service in the core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { NavigationComponent } from './core/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
+import { EnsureModuleLoadedOnceGuard } from './ensureModuleLoadedOnceGuard';
+import { RouterModule } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
+@NgModule({
+    imports: [ CommonModule, HttpClientModule ]
+})
 
 @NgModule({
-  imports: [ CommonModule, RouterModule ],
-  exports: [ ],
-  declarations: [ ],
-  providers: [ ]
+    imports: [CommonModule,
+              RouterModule,
+              HttpClientModule],
+    declarations: [NavigationComponent],
+    exports: [NavigationComponent],
+    providers: []
 })
-export class CoreModule { }
+export class CoreModule extends EnsureModuleLoadedOnceGuard {    // Ensure that CoreModule is only loaded into AppModule
+
+    // Looks for the module in the parent injector to see if it's already been loaded (only want it loaded once)
+    constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
+        super(parentModule);
+    }
+}
